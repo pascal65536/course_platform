3127d9a826c4b5f86cb7383aa63a34b9fe8afce5
pascal65536 <pascal65536@gmail.com>
2025-03-26 07:01:50 +0700

    t

--- dcr/script.py
+++ dcr/script.py
@@ -1,7 +1,7 @@
 # script.py
 import requests
 
-token = "7342084415:AAFYrhb2Rm_hYWKfTbcwZlZ_zaM_M6McWTw"
+token = "YOUR_BOT_TOKEN"
 key = "getUpdates"
 url = f"https://api.telegram.org/bot{token}/{key}"
 response = requests.get(url, timeout=30)
