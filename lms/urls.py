01093d8479b197321174ed594942e5cfb7e4d3b6
pascal65536 <pascal65536@gmail.com>
2025-04-15 21:33:56 +0700

    db.sqlite3

--- lms/urls.py
+++ lms/urls.py
@@ -5,7 +5,7 @@ from accounts.views import register, profile, custom_login, custom_logout
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', course_page, name='course_page'),
@@ -27,3 +27,24 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path("__debug__/", include(debug_toolbar.urls)),
+#     ] + urlpatterns
